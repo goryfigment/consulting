ed17beeeee28968053caf71c76c0428d8527aa71
Solomon Padilla <goryfigment@gmail.com>
2019-12-09 17:28:38 -0700

    change null to nothing

--- templates/handlebars/helpers/tableHandler.js
+++ templates/handlebars/helpers/tableHandler.js
@@ -3,7 +3,11 @@ module.exports = function(data, column) {
     var html = "";
 
     for (var i = 0; i < column.length; i++) {
-        html += "<td data-value='" + data[column[i]] + "' data-column='" + column[i] + "'>" + data[column[i]] + "</td>"
+        var rowData = data[column[i]];
+
+        rowData = rowData == null ? '' : rowData;
+
+        html += "<td data-value='" + data[column[i]] + "' data-column='" + column[i] + "'>" + rowData + "</td>"
     }
 
     return new Handlebars.SafeString(html);
